d83cd647e3f10b4d2b0265a1bf7c9d645a0cd233
Aditya Gupta <adiigupta3110@gmail.com>
2025-07-30 11:22:10 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,21 @@
-#include<iostream>
-using namespace std;
-int main(){
-    string a = "hello world";
-    cout<<a;
-    return 0;
+#include <stdio.h>
 
+int main() {
+    int num, reversed;
+    printf("Enter a 3-digit number: ");
+    scanf("%d", &num);
+
+    if (num < 100 || num > 999) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    int hundreds = num / 100;
+    int tens = (num / 10) % 10;
+    int units = num % 10;
+
+    reversed = units * 100 + tens * 10 + hundreds;
+    printf("Reversed number: %d\n", reversed);
+
+    return 0;
 }
